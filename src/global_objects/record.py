a65e5f9a35e1309f3a3adeaa37dd5da2a309f3ca
Agent-Husky <jannik.heym@gmx.de>
2024-11-04 00:44:53 +0100

    formatting Record + fixed print

--- src/global_objects/record.py
+++ src/global_objects/record.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+
 class Record(object):
     ttl: int = 0
     name: str = ""
@@ -7,12 +8,22 @@ class Record(object):
     type: str = ""
     id: Optional[str]
 
-    def __init__(self, type: str, name: str, value: str, ttl: int, id: str | None = None):
+    def __init__(
+        self, type: str, name: str, value: str, ttl: int, id: str | None = None
+    ):
         self.ttl = ttl
         self.name = name
         self.value = value
         self.type = type
         self.id = id
 
-    def __str__(self):
-        return "name: "+self.name+", value: "+self.value+", type: "+self.type+", ttl: "+str(self.ttl)+", id: "+str(self.id)
+    def __repr__(self):
+        return str(
+            {
+                "name": self.name,
+                "value": self.value,
+                "type": self.type,
+                "ttl": str(self.ttl),
+                "id": str(self.id),
+            }
+        )
