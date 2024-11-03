16c3e62314234855f244b2d4dafceb7a243125b9
Agent-Husky <jannik.heym@gmx.de>
2024-11-03 23:40:45 +0100

    combined json config files into one + added DNS record object

--- dns_updater.py
+++ dns_updater.py
@@ -1,20 +1,21 @@
 import requests
 import ipaddress as ipaddress
 import json as json
-from typing import List
+from typing import List, Optional
 
-ttl: int = 60
-prefix_offset: int = int("51", 16)
-api_token: str = "<token>"  # TODO: add your own token & tld in json files
 
-ipv4_config_file = open("ipv4-records.json", "r")
-ipv6_config_file = open("ipv6-records.json", "r")
+config_file = open("dns_config.json", "r")
+config = json.load(config_file)
+config_file.close()
 
-ipv4_config: list[dict[str, object]] = json.load(ipv4_config_file)
-ipv6_config: list[dict[str, object]] = json.load(ipv6_config_file)
 
-ipv4_config_file.close()
-ipv6_config_file.close()
+hetzner_api_token: str = config["provider_config"]["api_token"]  # TODO: add your own token & tld in json files
+ttl: int = config["ttl"]
+
+ipv4_config: list[dict[str, object]] = config["ipv4"]["config"]
+prefix_offset: int = int(config["ipv6"]["prefix_offset"], 16)
+ipv6_config: list[dict[str, object]] = config["ipv6"]["config"]
+
 
 ipv4: str = requests.get("https://api.ipify.org").text
 ipv6: str = requests.get("https://api6.ipify.org").text
@@ -25,7 +26,7 @@ ipv6_parts: list[int] = list(map(lambda x: int(x, 16), ipv6_exploded.split(sep="
 zones = requests.get(
     url="https://dns.hetzner.com/api/v1/zones",
     headers={
-        "Auth-API-Token": api_token,
+        "Auth-API-Token": hetzner_api_token,
     },
 )
 
@@ -34,6 +35,29 @@ zone_ids: dict[str, str] = {}
 
 for entry in zones.json()["zones"]:
     zone_ids[entry["name"]] = entry["id"]
+
+class Record(object):
+    ttl: int = 0
+    name: str = ""
+    value: str = ""
+    type: str = ""
+    id: Optional[str]
+
+    def __init__(self, type: str, name: str, value: str, ttl: int, id: str | None = None):
+        self.ttl = ttl
+        self.name = name
+        self.value = value
+        self.type = type
+        self.id = id
+
+    def __str__(self):
+        return "name: "+self.name+", value: "+self.value+", type: "+self.type+", ttl: "+str(self.ttl)+", id: "+str(self.id)
+
+temp: Record = Record(type="A", name="test", value="1.2.3.4", ttl=ttl)
+
+print(temp)
+
+print(prefix_offset)
 
 print(zone_ids)
 print()
