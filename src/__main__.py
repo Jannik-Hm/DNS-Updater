9926175d96fceaaaaf28975f4ffc160eb5f20bc7
Agent-Husky <jannik.heym@gmx.de>
2024-11-04 21:10:05 +0100

    implemented new objects + funcs, reworked config, started hetzner update/create record data

--- src/__main__.py
+++ src/__main__.py
@@ -4,7 +4,7 @@ import json as json
 
 from typing import Optional
 
-from global_objects import Record
+from global_objects import Record, dnsV4Config, dnsV4ConfigRecord, dnsV6Config, dnsV6ConfigRecord, globalConfig
 import providers
 import providers.hetzner
 
@@ -12,7 +12,7 @@ config_file = open("dns_config.json", "r")
 config = json.load(config_file)
 config_file.close()
 
-ttl: int = config["global"]["ttl"]
+global_config: globalConfig = globalConfig(ttl=config["global"]["ttl"], prefix_offset=config["global"]["prefix_offset"])
 
 ipv4: Optional[str] = None
 ipv6: Optional[str] = None
@@ -31,19 +31,27 @@ for provider in config["providers"]:
 # TODO: add your own token & tld in json files
     print(provider)
 
-    ipv4_config: list[dict[str, object]] = provider["ipv4"]["config"]
+    ipv4_config: list[dnsV4Config] = []
+    ipv6_config: list[dnsV6Config] = []
     prefix_offset: int = int(config["global"]["prefix_offset"], 16)
-    ipv6_config: list[dict[str, object]] = provider["ipv6"]["config"]
 
-    if(ipv6 != None):
+    for zone in provider["zones"]:
+
+        ipv4_config.append(dnsV4Config(zone["name"], list(map(lambda x: dnsV4ConfigRecord(name=x["name"]), zone["ipv4_records"]))))
+
+        ipv6_config.append(dnsV6Config(zone["name"], list(map(lambda x: dnsV6ConfigRecord(name=x["name"],prefixOffset=x["prefixOffset"],suffix=x["suffix"]), zone["ipv6_records"]))))
+
+    #ipv6_config: list[dict[str, object]] = provider["ipv6"]["config"]
+
+    if(ipv6 is not None):
         ipv6_exploded: str = ipaddress.IPv6Address(ipv6).exploded
-        ipv6_parts: list[int] = list(map(lambda x: int(x, 16), ipv6_exploded.split(sep=":")))
+        ipv6_parts: list[str] = ipv6_exploded.split(sep=":")
 
     provider_config = provider["provider_config"]
 
-    match provider:
+    match provider["provider"]:
         case "hetzner":
-            providers.hetzner.updateHetznerEntries(providerConfig=provider_config)
+            providers.hetzner.updateHetznerEntries(providerConfig=provider_config, dnsV4Config=ipv4_config, ipv4Address=ipv4, dnsV6Config=ipv6_config, ipv6Prefix=ipv6_parts[:4], globalConfig=global_config)
 
     print(prefix_offset)
 
--- src/providers/hetzner.py
+++ src/providers/hetzner.py
@@ -1,14 +1,22 @@
 # from typing import Dict
 import requests
-from global_objects import Record
+from global_objects import (
+    Record,
+    dnsV4Config,
+    dnsV4ConfigRecord,
+    dnsV6Config,
+    dnsV6ConfigRecord,
+    globalConfig,
+)
+from helper_functions import ipv6
 
 
 # TODO: maybe add to global_objects if it can be reused
 class Zone_Data(object):
     name: str = ""
-    records: list[Record] = []
+    records: dict[str, Record] = {}
 
-    def __init__(self, name: str, records: list[Record]):
+    def __init__(self, name: str, records: dict[str, Record]):
         self.name = name
         self.records = records
 
@@ -16,7 +24,14 @@ class Zone_Data(object):
         return str({"name": self.name, "records": self.records})
 
 
-def updateHetznerEntries(providerConfig: dict[str, str]) -> None:
+def updateHetznerEntries(
+    providerConfig: dict[str, str],
+    dnsV4Config: list[dnsV4Config],
+    dnsV6Config: list[dnsV6Config],
+    globalConfig: globalConfig,
+    ipv4Address: str | None = None,
+    ipv6Prefix: list[str] | None = None,
+) -> None:
     hetzner_api_token: str = providerConfig["api_token"]
 
     getZones = requests.get(
@@ -27,9 +42,11 @@ def updateHetznerEntries(providerConfig: dict[str, str]) -> None:
     )
     # zone_ids: List[str] = zones.json()["zones"]
     dns_data: dict[str, Zone_Data] = {}
+    zone_id_map: dict[str, str] = {}
 
     for entry in getZones.json()["zones"]:
-        dns_data[entry["id"]] = Zone_Data(name=entry["name"], records=[])
+        dns_data[entry["id"]] = Zone_Data(name=entry["name"], records={})
+        zone_id_map[entry["name"]] = entry["id"]
 
     getRecords = requests.get(
         url="https://dns.hetzner.com/api/v1/records",
@@ -39,17 +56,94 @@ def updateHetznerEntries(providerConfig: dict[str, str]) -> None:
     )
 
     for entry in getRecords.json()["records"]:
-        if entry["type"] == "A" or entry["type"] == "AAAA":
-            dns_data[entry["zone_id"]].records.append(
+        if (entry["type"] == "A" and ipv4Address is not None) or (
+            entry["type"] == "AAAA" and ipv6Prefix is not None and ipv6Prefix.__len__() >= 4
+        ):
+            dns_data[entry["zone_id"]].records[entry["type"] + "-" + entry["name"]] = (
                 Record(
                     type=entry["type"],
                     id=entry["id"],
+                    zone_id=entry["zone_id"],
                     name=entry["name"],
                     ttl=entry["ttl"],
                     value=entry["value"],
                 )
             )
 
+    createRecordsBody: list = []
+    updateRecordsBody: list = []
+
+    if ipv4Address is not None:
+        for zone in dnsV4Config:
+            zone_id: str = zone_id_map[zone.zone]
+            for record in zone.records:
+                if dns_data[zone_id].records.__contains__("A-" + record.name):
+                    updateRecordsBody.append(
+                        {
+                            "id": dns_data[zone_id].records["A-" + record.name].id,
+                            "zone_id": zone_id,
+                            "type": "A",
+                            "name": record.name,
+                            "value": ipv4Address,
+                            "ttl": globalConfig.ttl,
+                        }
+                    )
+                else:
+                    createRecordsBody.append(
+                        {
+                            "zone_id": zone_id,
+                            "type": "A",
+                            "name": record.name,
+                            "value": ipv4Address,
+                            "ttl": globalConfig.ttl,
+                        }
+                    )
+
+    if ipv6Prefix is not None and ipv6Prefix.__len__() >= 4:
+        for zone in dnsV6Config:
+            zone_id: str = zone_id_map[zone.zone]
+            for record in zone.records:
+                dns_record_data = dns_data[zone_id].records["AAAA-" + record.name]
+                record_exists: bool = dns_data[zone_id].records.__contains__(
+                    "AAAA-" + record.name
+                )
+                ipv6Address: str = ipv6.calculateIPv6Address(
+                    prefix=ipv6Prefix,
+                    prefixOffset=record.prefixOffset,
+                    currentAddressOrFixedSuffix=(record.suffix or dns_data[zone_id].records["AAAA-" + record.name].value)
+                )
+                print("IPv6 Address:")
+                print(ipv6Address)
+                if record_exists:
+                    updateRecordsBody.append(
+                        {
+                            "id": dns_data[zone_id].records["AAAA-" + record.name].id,
+                            "zone_id": zone_id,
+                            "type": "AAAA",
+                            "name": record.name,
+                            "value": ipv6Address,
+                            "ttl": globalConfig.ttl,
+                        }
+                    )
+                else:
+                    createRecordsBody.append(
+                        {
+                            "zone_id": zone_id,
+                            "type": "AAAA",
+                            "name": record.name,
+                            "value": ipv6Address,
+                            "ttl": globalConfig.ttl,
+                        }
+                    )
+
     print(dns_data)
+
+    print()
+
+    print(dnsV4Config)
+
+    print()
+
+    print(dnsV6Config)
 
     return
