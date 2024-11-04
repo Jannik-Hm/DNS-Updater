80316375fb2cbd18a87d289f4037e53a6103cd01
Agent-Husky <jannik.heym@gmx.de>
2024-11-04 21:07:19 +0100

    added zone_id to record object

--- src/global_objects/record.py
+++ src/global_objects/record.py
@@ -7,15 +7,17 @@ class Record(object):
     value: str = ""
     type: str = ""
     id: Optional[str]
+    zone_id: Optional[str]
 
     def __init__(
-        self, type: str, name: str, value: str, ttl: int, id: str | None = None
+        self, type: str, name: str, value: str, ttl: int, id: str | None = None, zone_id: str | None = None
     ):
         self.ttl = ttl
         self.name = name
         self.value = value
         self.type = type
         self.id = id
+        self.zone_id = zone_id
 
     def __repr__(self):
         return str(
@@ -25,5 +27,6 @@ class Record(object):
                 "type": self.type,
                 "ttl": str(self.ttl),
                 "id": str(self.id),
+                "zone_id": str(self.zone_id)
             }
         )
