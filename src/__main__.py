efded96f59ebe61a32c577a1705e264595d64ba7
Agent-Husky <jannik.heym@gmx.de>
2024-11-06 11:17:54 +0100

    added dryrun options + try catch for timeout and connectionerrors

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,7 @@
 import requests
 import ipaddress as ipaddress
 import yaml as yaml
+import argparse
 
 from typing import Optional
 
@@ -14,6 +15,15 @@ from global_objects import (
 import providers
 import providers.hetzner
 from helper_functions import ipv6
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-d", "--dryrun", action='store_true', dest="dryrun")
+
+args = parser.parse_args()
+
+if args.dryrun:
+    print("This is a dryrun. No Updates will be applied.")
 
 config_file = open("dns_config.yaml", "r")
 config = yaml.safe_load(config_file)
@@ -31,6 +41,8 @@ try:
     ipv4Address = requests.get("https://api.ipify.org", timeout=5).text
 except requests.exceptions.ConnectTimeout:
     print("Timeout")  # TODO: perform Logging of timeout
+except requests.exceptions.ConnectionError:
+    print("Unable to establish connection")
 try:
     ipv6Address = requests.get("https://api6.ipify.org", timeout=5).text
     if ipv6Address is not None:
@@ -41,51 +53,54 @@ try:
         ).split(":")
 except requests.exceptions.ConnectTimeout:
     print("Timeout")  # TODO: perform Logging of timeout
+except requests.exceptions.ConnectionError:
+    print("Unable to establish connection")
 
+if ipv4Address is not None or ipv6Address is not None:
+    for provider in config["providers"]:
+        ipv4_config: list[dnsV4Config] = []
+        ipv6_config: list[dnsV6Config] = []
 
-for provider in config["providers"]:
-    ipv4_config: list[dnsV4Config] = []
-    ipv6_config: list[dnsV6Config] = []
+        for zone in provider["zones"]:
 
-    for zone in provider["zones"]:
-
-        ipv4_config.append(
-            dnsV4Config(
-                zone["name"],
-                list(
-                    map(
-                        lambda x: dnsV4ConfigRecord(name=x["name"]),
-                        zone["ipv4_records"],
-                    )
-                ),
+            ipv4_config.append(
+                dnsV4Config(
+                    zone["name"],
+                    list(
+                        map(
+                            lambda x: dnsV4ConfigRecord(name=x["name"]),
+                            zone["ipv4_records"],
+                        )
+                    ),
+                )
             )
-        )
 
-        ipv6_config.append(
-            dnsV6Config(
-                zone["name"],
-                list(
-                    map(
-                        lambda x: dnsV6ConfigRecord(
-                            name=x["name"],
-                            prefixOffset=x["prefixOffset"],
-                            suffix=x["suffix"],
-                        ),
-                        zone["ipv6_records"],
-                    )
-                ),
+            ipv6_config.append(
+                dnsV6Config(
+                    zone["name"],
+                    list(
+                        map(
+                            lambda x: dnsV6ConfigRecord(
+                                name=x["name"],
+                                prefixOffset=x["prefixOffset"],
+                                suffix=x["suffix"],
+                            ),
+                            zone["ipv6_records"],
+                        )
+                    ),
+                )
             )
-        )
 
-    provider_config = provider["provider_config"]
+        provider_config = provider["provider_config"]
 
-    match provider["provider"]:
-        case "hetzner":
-            providers.hetzner.updateHetznerEntries(
-                providerConfig=provider_config,
-                dnsV4Config=ipv4_config,
-                ipv4Address=ipv4Address,
-                dnsV6Config=ipv6_config,
-                ipv6Prefix=ipv6Prefix,
-                globalConfig=global_config,
-            )
+        match provider["provider"]:
+            case "hetzner":
+                providers.hetzner.updateHetznerEntries(
+                    providerConfig=provider_config,
+                    dnsV4Config=ipv4_config,
+                    ipv4Address=ipv4Address,
+                    dnsV6Config=ipv6_config,
+                    ipv6Prefix=ipv6Prefix,
+                    globalConfig=global_config,
+                    dryrun=args.dryrun
+                )
--- src/providers/hetzner.py
+++ src/providers/hetzner.py
@@ -9,6 +9,7 @@ from global_objects import (
 from helper_functions import ipv6
 import json as json
 
+
 def updateHetznerEntries(
     providerConfig: dict[str, str],
     dnsV4Config: list[dnsV4Config],
@@ -16,88 +17,103 @@ def updateHetznerEntries(
     globalConfig: globalConfig,
     ipv4Address: str | None = None,
     ipv6Prefix: list[str] | None = None,
+    dryrun: bool | None = None,
 ) -> None:
     hetzner_api_token: str = providerConfig["api_token"]
 
     # TODO: log errors instead of just printing
-    getZones = requests.get(
-        url="https://dns.hetzner.com/api/v1/zones",
-        headers={
-            "Auth-API-Token": hetzner_api_token,
-        },
-        timeout=5,
-    )
-    # zone_ids: List[str] = zones.json()["zones"]
-    dns_records_data: dict[str, dict[str, Record]] = {}
-    zone_id_map: dict[str, str] = {}
-
-    if getZones.status_code != 200:
-        match getZones.status_code:
-            case 400:
-                print("Get Zones - Pagination selectors are mutually exclusive")
-                return
-            case 401:
-                print("Get Zones - " + getZones.reason)
-                return
-            case 406:
-                print("Get Zones - " + getZones.reason)
-                return
-    else:
-        for entry in getZones.json()["zones"]:
-            dns_records_data[entry["id"]] = {}
-            zone_id_map[entry["name"]] = entry["id"]
-
-        getRecords = requests.get(
-            url="https://dns.hetzner.com/api/v1/records",
+    try:
+        getZones = requests.get(
+            url="https://dns.hetzner.com/api/v1/zones",
             headers={
                 "Auth-API-Token": hetzner_api_token,
             },
-            timeout=10,  # wait longer for bigger responses in case of a lot of records
+            timeout=5,
         )
+        dns_records_data: dict[str, dict[str, Record]] = {}
+        zone_id_map: dict[str, str] = {}
 
-        if getRecords.status_code != 200:
-            match getRecords.status_code:
+        if getZones.status_code != 200:
+            match getZones.status_code:
+                case 400:
+                    print("Get Zones - Pagination selectors are mutually exclusive")
+                    return
                 case 401:
-                    print("Get Records - " + getRecords.reason)
+                    print("Get Zones - " + getZones.reason)
                     return
                 case 406:
-                    print("Get Records - " + getRecords.reason)
+                    print("Get Zones - " + getZones.reason)
                     return
         else:
-            for entry in getRecords.json()["records"]:
-                if (entry["type"] == "A" and ipv4Address is not None) or (
-                    entry["type"] == "AAAA"
-                    and ipv6Prefix is not None
-                    and ipv6Prefix.__len__() >= 4
-                ):
-                    dns_records_data[entry["zone_id"]][
-                        entry["type"] + "-" + entry["name"]
-                    ] = Record(
-                        type=entry["type"],
-                        id=entry["id"],
-                        zone_id=entry["zone_id"],
-                        name=entry["name"],
-                        ttl=entry["ttl"],
-                        value=entry["value"],
-                    )
+            for entry in getZones.json()["zones"]:
+                dns_records_data[entry["id"]] = {}
+                zone_id_map[entry["name"]] = entry["id"]
 
-            createRecordsBody: list = []
-            updateRecordsBody: list = []
+            getRecords = requests.get(
+                url="https://dns.hetzner.com/api/v1/records",
+                headers={
+                    "Auth-API-Token": hetzner_api_token,
+                },
+                timeout=10,  # wait longer for bigger responses in case of a lot of records
+            )
 
-            if ipv4Address is not None:
-                for zone in dnsV4Config:
-                    zone_id: str = zone_id_map[zone.zone]
-                    for record in zone.records:
-                        if dns_records_data[zone_id].__contains__("A-" + record.name):
-                            if (
-                                ipv4Address
-                                != dns_records_data[zone_id]["A-" + record.name].value
+            if getRecords.status_code != 200:
+                match getRecords.status_code:
+                    case 401:
+                        print("Get Records - " + getRecords.reason)
+                        return
+                    case 406:
+                        print("Get Records - " + getRecords.reason)
+                        return
+            else:
+                for entry in getRecords.json()["records"]:
+                    if (entry["type"] == "A" and ipv4Address is not None) or (
+                        entry["type"] == "AAAA"
+                        and ipv6Prefix is not None
+                        and ipv6Prefix.__len__() >= 4
+                    ):
+                        dns_records_data[entry["zone_id"]][
+                            entry["type"] + "-" + entry["name"]
+                        ] = Record(
+                            type=entry["type"],
+                            id=entry["id"],
+                            zone_id=entry["zone_id"],
+                            name=entry["name"],
+                            ttl=entry["ttl"],
+                            value=entry["value"],
+                        )
+
+                createRecordsBody: list = []
+                updateRecordsBody: list = []
+
+                if ipv4Address is not None:
+                    for zone in dnsV4Config:
+                        zone_id: str = zone_id_map[zone.zone]
+                        for record in zone.records:
+                            if dns_records_data[zone_id].__contains__(
+                                "A-" + record.name
                             ):
-                                updateRecordsBody.append(
+                                if (
+                                    ipv4Address
+                                    != dns_records_data[zone_id][
+                                        "A-" + record.name
+                                    ].value
+                                ):
+                                    updateRecordsBody.append(
+                                        {
+                                            "id": dns_records_data[zone_id][
+                                                "A-" + record.name
+                                            ].id,
+                                            "zone_id": zone_id,
+                                            "type": "A",
+                                            "name": record.name,
+                                            "value": ipv4Address,
+                                            "ttl": globalConfig.ttl,
+                                        }
+                                    )
+                            else:
+                                createRecordsBody.append(
                                     {
-                                        "id": dns_records_data[zone_id][
-                                            "A-" + record.name
-                                        ].id,
                                         "zone_id": zone_id,
                                         "type": "A",
                                         "name": record.name,
@@ -105,46 +121,46 @@ def updateHetznerEntries(
                                         "ttl": globalConfig.ttl,
                                     }
                                 )
-                        else:
-                            createRecordsBody.append(
-                                {
-                                    "zone_id": zone_id,
-                                    "type": "A",
-                                    "name": record.name,
-                                    "value": ipv4Address,
-                                    "ttl": globalConfig.ttl,
-                                }
-                            )
 
-            if ipv6Prefix is not None and ipv6Prefix.__len__() >= 4:
-                for zone in dnsV6Config:
-                    zone_id: str = zone_id_map[zone.zone]
-                    for record in zone.records:
-                        record_exists: bool = dns_records_data[zone_id].__contains__(
-                            "AAAA-" + record.name
-                        )
-                        ipv6Address: str = ipv6.calculateIPv6Address(
-                            prefix=ipv6Prefix,
-                            prefixOffset=record.prefixOffset,
-                            currentAddressOrFixedSuffix=(
-                                record.suffix
-                                or dns_records_data[zone_id][
-                                    "AAAA-" + record.name
-                                ].value
-                            ),
-                        )
-                        if record_exists:
-                            if (
-                                ipv6Address
-                                != dns_records_data[zone_id][
-                                    "AAAA-" + record.name
-                                ].value
-                            ):
-                                updateRecordsBody.append(
+                if ipv6Prefix is not None and ipv6Prefix.__len__() >= 4:
+                    for zone in dnsV6Config:
+                        zone_id: str = zone_id_map[zone.zone]
+                        for record in zone.records:
+                            record_exists: bool = dns_records_data[
+                                zone_id
+                            ].__contains__("AAAA-" + record.name)
+                            ipv6Address: str = ipv6.calculateIPv6Address(
+                                prefix=ipv6Prefix,
+                                prefixOffset=record.prefixOffset,
+                                currentAddressOrFixedSuffix=(
+                                    record.suffix
+                                    or dns_records_data[zone_id][
+                                        "AAAA-" + record.name
+                                    ].value
+                                ),
+                            )
+                            if record_exists:
+                                if (
+                                    ipv6Address
+                                    != dns_records_data[zone_id][
+                                        "AAAA-" + record.name
+                                    ].value
+                                ):
+                                    updateRecordsBody.append(
+                                        {
+                                            "id": dns_records_data[zone_id][
+                                                "AAAA-" + record.name
+                                            ].id,
+                                            "zone_id": zone_id,
+                                            "type": "AAAA",
+                                            "name": record.name,
+                                            "value": ipv6Address,
+                                            "ttl": globalConfig.ttl,
+                                        }
+                                    )
+                            else:
+                                createRecordsBody.append(
                                     {
-                                        "id": dns_records_data[zone_id][
-                                            "AAAA-" + record.name
-                                        ].id,
                                         "zone_id": zone_id,
                                         "type": "AAAA",
                                         "name": record.name,
@@ -152,77 +168,91 @@ def updateHetznerEntries(
                                         "ttl": globalConfig.ttl,
                                     }
                                 )
-                        else:
-                            createRecordsBody.append(
-                                {
-                                    "zone_id": zone_id,
-                                    "type": "AAAA",
-                                    "name": record.name,
-                                    "value": ipv6Address,
-                                    "ttl": globalConfig.ttl,
-                                }
-                            )
 
-            print(updateRecordsBody)
+                if dryrun:
+                    print()
+                    print("These Records would be updated:")
+                    print(json.dumps(updateRecordsBody))
+                    print()
+                elif updateRecordsBody.__len__() > 0:
+                    updateResponse = requests.put(
+                        url="https://dns.hetzner.com/api/v1/records/bulk",
+                        headers={
+                            "Content-Type": "application/json",
+                            "Auth-API-Token": hetzner_api_token,
+                        },
+                        data=json.dumps({"records": updateRecordsBody}),
+                        timeout=10,
+                    )
 
-            if updateRecordsBody.__len__() > 0:
-                updateResponse = requests.put(
-                    url="https://dns.hetzner.com/api/v1/records/bulk",
-                    headers={
-                        "Content-Type": "application/json",
-                        "Auth-API-Token": hetzner_api_token,
-                    },
-                    data=json.dumps({"records": updateRecordsBody}),
-                    timeout=10,
-                )
+                    if updateResponse.status_code != 200:
+                        match updateResponse.status_code:
+                            case 401:
+                                print(
+                                    "Update A Records Error - " + updateResponse.reason
+                                )
+                            case 403:
+                                print(
+                                    "Update A Records Error - " + updateResponse.reason
+                                )
+                            case 406:
+                                print(
+                                    "Update A Records Error - " + updateResponse.reason
+                                )
+                            case 422:
+                                print("Update A Records Error - Unprocessable entity")
 
-                if updateResponse.status_code != 200:
-                    match updateResponse.status_code:
-                        case 401:
-                            print("Update A Records Error - " + updateResponse.reason)
-                        case 403:
-                            print("Update A Records Error - " + updateResponse.reason)
-                        case 406:
-                            print("Update A Records Error - " + updateResponse.reason)
-                        case 422:
-                            print("Update A Records Error - Unprocessable entity")
+                if dryrun:
+                    print()
+                    print("These Records would be created:")
+                    print(json.dumps(createRecordsBody))
+                    print()
+                elif createRecordsBody.__len__() > 0:
+                    createResponse = requests.post(
+                        url="https://dns.hetzner.com/api/v1/records/bulk",
+                        headers={
+                            "Content-Type": "application/json",
+                            "Auth-API-Token": hetzner_api_token,
+                        },
+                        data=json.dumps({"records": createRecordsBody}),
+                        timeout=10,
+                    )
 
-            print(createRecordsBody)
-
-            if createRecordsBody.__len__() > 0:
-                createResponse = requests.post(
-                    url="https://dns.hetzner.com/api/v1/records/bulk",
-                    headers={
-                        "Content-Type": "application/json",
-                        "Auth-API-Token": hetzner_api_token,
-                    },
-                    data=json.dumps({"records": createRecordsBody}),
-                    timeout=10,
-                )
-
-                if createResponse.status_code != 200:
-                    match createResponse.status_code:
-                        case 401:
-                            print(
-                                "Update AAAA Records Error - " + createResponse.reason
-                            )
-                        case 403:
-                            print(
-                                "Update AAAA Records Error - " + createResponse.reason
-                            )
-                        case 404:
-                            print(
-                                "Update AAAA Records Error - " + createResponse.reason
-                            )
-                        case 406:
-                            print(
-                                "Update AAAA Records Error - " + createResponse.reason
-                            )
-                        case 409:
-                            print(
-                                "Update AAAA Records Error - " + createResponse.reason
-                            )
-                        case 422:
-                            print("Update AAAA Records Error - Unprocessable entity")
+                    if createResponse.status_code != 200:
+                        match createResponse.status_code:
+                            case 401:
+                                print(
+                                    "Update AAAA Records Error - "
+                                    + createResponse.reason
+                                )
+                            case 403:
+                                print(
+                                    "Update AAAA Records Error - "
+                                    + createResponse.reason
+                                )
+                            case 404:
+                                print(
+                                    "Update AAAA Records Error - "
+                                    + createResponse.reason
+                                )
+                            case 406:
+                                print(
+                                    "Update AAAA Records Error - "
+                                    + createResponse.reason
+                                )
+                            case 409:
+                                print(
+                                    "Update AAAA Records Error - "
+                                    + createResponse.reason
+                                )
+                            case 422:
+                                print(
+                                    "Update AAAA Records Error - Unprocessable entity"
+                                )
+    except requests.exceptions.Timeout:
+        print("Get Zones - Timeout")
+    except requests.exceptions.ConnectionError:
+        print("Unable to establish connection")
+    # zone_ids: List[str] = zones.json()["zones"]
 
     return
