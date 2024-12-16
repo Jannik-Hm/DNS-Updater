54bd4b683945c263b817478d58c997e12e4c7f7b
Agent-Husky <jannik.heym@gmx.de>
2024-12-16 12:03:30 +0100

    prefix ids in the opnsense gui are set in decimal apparently, updating the prefix calc accordingly

--- src/helper_functions/ipv6.py
+++ src/helper_functions/ipv6.py
@@ -2,7 +2,7 @@ import ipaddress as ipaddress
 
 def calculateIPv6Address(prefix: list[str], prefixOffset: str, currentAddressOrFixedSuffix: str) -> str:
   prefix_int = int("".join(prefix[:4]), 16)
-  prefix_id_int = int(prefixOffset, 16)
+  prefix_id_int = int(prefixOffset, 10)
   if f"{(prefix_int + prefix_id_int):016x}".__len__() > 16:
     raise ValueError(f"The generated prefix for base prefix {':'.join(prefix[:4])} and prefixOffset {prefixOffset} is overflowing. Please check your config.")
   new_prefix = f"{(prefix_int + prefix_id_int) & 0xFFFFFFFFFFFFFFFF:016x}"
