47a31d43e9cab3460b1ebe1c2a9fc6cd533a92eb
Jannik-Hm <jannik.heym@gmx.de>
2025-09-23 09:56:31 +0200

    update main to use new provider (WIP)

--- src/__main__.py
+++ src/__main__.py
@@ -5,16 +5,10 @@ import argparse
 
 from typing import Optional
 
-from global_objects import (
-    dnsV4Config,
-    dnsV4ConfigRecord,
-    dnsV6Config,
-    dnsV6ConfigRecord,
-    globalConfig,
-)
 import providers
-import providers.hetzner
 from helper_functions import ipv6, logging
+
+from global_objects.config import Config
 
 parser = argparse.ArgumentParser()
 
@@ -26,16 +20,18 @@ parser.add_argument("--disable-ipv6", dest="disable_ipv6", action='store_true', 
 args = parser.parse_args()
 
 config_file = open(args.config_path, "r")
-config = yaml.safe_load(config_file)
+config_json = yaml.safe_load(config_file)
 config_file.close()
 
-global_config: globalConfig = globalConfig(
-    ttl=config["global"]["ttl"], prefix_offset=config["global"]["current_prefix_offset"]
-)
+# global_config: globalConfig = globalConfig(
+#     ttl=config_json["global"]["ttl"], prefix_offset=config_json["global"]["current_prefix_offset"]
+# )
 
-dryrun: bool = args.dryrun or config["global"]["dry-run"]
-disable_ipv4: bool = args.disable_ipv4 or config["global"]["disable-ipv4"]
-disable_ipv6: bool = args.disable_ipv6 or config["global"]["disable-ipv6"]
+config: Config = Config.model_validate(config_json)
+
+dryrun: bool = args.dryrun or config.global_.dry_run
+disable_ipv4: bool = args.disable_ipv4 or config.global_.disable_v4
+disable_ipv6: bool = args.disable_ipv6 or config.global_.disable_v6
 
 ipv4Address: Optional[str] = None
 ipv6Address: Optional[str] = None
@@ -43,13 +39,15 @@ ipv6Prefix: Optional[list[str]] = None
 
 logProviders: list[logging.LogProvider] = []
 
-for logProvider in config["global"]["logging"]:
-    loglevel: logging.LogLevel = logging.LogLevel.fromString(logProvider["loglevel"])
-    match str.lower(logProvider["provider"]):
+for logProvider in config.global_.logging:
+    loglevel: logging.LogLevel = logging.LogLevel.fromString(logProvider.loglevel)
+    match str.lower(logProvider.provider):
         case "print":
             logProviders.append(logging.PrintLogger(loglevel=loglevel))
         case "discord":
-            logProviders.append(logging.DiscordLogger(webhook_url=logProvider["provider_config"]["webhook_url"],loglevel=loglevel))
+            if logProvider.provider_config is not None:
+                # TODO: validate discord logger config
+                logProviders.append(logging.DiscordLogger(webhook_url=logProvider.provider_config["webhook_url"],loglevel=loglevel))
 
 logger = logging.Logger(logProviders=logProviders)
 
@@ -68,7 +66,7 @@ if not disable_ipv6:
         if ipv6Address is not None:
             ipv6Prefix = ipv6.calculateIPv6Address(
                 prefix=ipaddress.IPv6Address(ipv6Address).exploded.split(":"),
-                prefixOffset="-" + str(global_config.prefix_offset), # negative Offset
+                prefixOffset="-" + str(config.global_.current_prefix_offset), # negative Offset
                 currentAddressOrFixedSuffix="::",
             ).split(":")
     except requests.exceptions.ConnectTimeout:
@@ -79,51 +77,15 @@ if not disable_ipv6:
         logger.log(message=str(e.args), loglevel=logging.LogLevel.FATAL)
 
 if ipv4Address is not None or ipv6Address is not None:
-    for provider in config["providers"]:
-        ipv4_config: list[dnsV4Config] = []
-        ipv6_config: list[dnsV6Config] = []
+    for provider in config.providers:
 
-        for zone in provider["zones"]:
-
-            ipv4_config.append(
-                dnsV4Config(
-                    zone["name"],
-                    list(
-                        map(
-                            lambda x: dnsV4ConfigRecord(name=x["name"]),
-                            zone["ipv4_records"],
-                        )
-                    ),
-                )
-            )
-
-            ipv6_config.append(
-                dnsV6Config(
-                    zone["name"],
-                    list(
-                        map(
-                            lambda x: dnsV6ConfigRecord(
-                                name=x["name"],
-                                prefixOffset=x["prefixOffset"],
-                                suffix=x["suffix"],
-                            ),
-                            zone["ipv6_records"],
-                        )
-                    ),
-                )
-            )
-
-        provider_config = provider["provider_config"]
-
-        match provider["provider"]:
+        match provider.provider:
             case "hetzner":
-                providers.hetzner.updateHetznerEntries(
-                    providerConfig=provider_config,
-                    dnsV4Config=ipv4_config,
-                    ipv4Address=ipv4Address,
-                    dnsV6Config=ipv6_config,
-                    ipv6Prefix=ipv6Prefix,
-                    globalConfig=global_config,
-                    dryrun=dryrun,
-                    logger=logger
-                )
+
+                provider = providers.HetznerProvider(providerConfig=provider, globalConfig=config.global_, logger=logger)
+
+                provider.getCurrentDNSConfig()
+
+                provider.updateDNSRecordsLocally(currentIPv4=ipv4Address, currentIPv6Prefix=ipv6Address.split(":") if ipv6Address else None)
+
+                provider.updateDNSConfig()
