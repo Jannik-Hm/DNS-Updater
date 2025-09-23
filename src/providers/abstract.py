ed037d386d5dfcc5397698278177f6c743552d2c
Jannik-Hm <jannik.heym@gmx.de>
2025-09-23 23:19:40 +0200

    reduced code duplication in abstract provider + formatting

--- src/providers/abstract.py
+++ src/providers/abstract.py
@@ -7,7 +7,8 @@ from config.config_models import ProviderConfig, GlobalConfig
 from helper_functions import logging
 from ip_fetching import calculateIPv6Address
 
-#TODO: allow consecutive fails (for timeout cases)
+# TODO: allow consecutive fails (for timeout cases)
+
 
 class Record(BaseModel):
     ttl: int | None = None
@@ -27,7 +28,12 @@ class Provider(ABC):
     updated_zone_records: dict[str, dict[str, Record]] = {}
     created_zone_records: dict[str, dict[str, Record]] = {}
 
-    def __init__(self, providerConfig: ProviderConfig[Any], globalConfig: GlobalConfig, logger: logging.Logger):
+    def __init__(
+        self,
+        providerConfig: ProviderConfig[Any],
+        globalConfig: GlobalConfig,
+        logger: logging.Logger,
+    ):
         self.config = self.validateConfig(providerConfig)
         self.globalConfig = globalConfig
         self.logger = logger
@@ -46,86 +52,74 @@ class Provider(ABC):
             ttl=60, name=name, value=value, type=type
         )
 
+    def _updateSingleDNSRecordLocally(
+        self, zoneName: str, recordName: str, type: str, value: str
+    ) -> bool:
+        if f"{type}-{recordName}" in self.zone_records[self.zone_ids[zoneName]]:
+            temp_record = self.zone_records[self.zone_ids[zoneName]][
+                f"{type}-{recordName}"
+            ]
+
+            if temp_record.value == value and temp_record.ttl == self.globalConfig.ttl:
+                # skip if record value is unchanged
+                return False
+
+            temp_record.value = value
+            temp_record.ttl = self.globalConfig.ttl
+            if not self.zone_ids[zoneName] in self.updated_zone_records:
+                self.updated_zone_records[self.zone_ids[zoneName]] = {}
+            self.updated_zone_records[self.zone_ids[zoneName]][
+                f"{type}-{recordName}"
+            ] = temp_record
+        else:
+            self.createDNSRecord(
+                type=type,
+                name=recordName,
+                value=value,
+                zoneName=zoneName,
+            )
+        return True
+
     def updateDNSRecordsLocally(
         self, currentIPv4: str | None, currentIPv6Prefix: list[str] | None
     ):
         for zone in self.config.zones:
             if not self.globalConfig.disable_v4 and currentIPv4:
                 for record in zone.ipv4_records:
-                    if (
-                        f"AAAA-{record.name}"
-                        in self.zone_records[self.zone_ids[zone.name]]
-                    ):
-                        temp_record = self.zone_records[self.zone_ids[zone.name]][
-                            f"A-{record.name}"
-                        ]
-
-                        if temp_record.value == currentIPv4:
-                            continue
-
-                        temp_record.value = currentIPv4
-                        if not self.zone_ids[zone.name] in self.updated_zone_records:
-                            self.updated_zone_records[self.zone_ids[zone.name]] = {}
-                        self.updated_zone_records[self.zone_ids[zone.name]][
-                            f"A-{record.name}"
-                        ] = temp_record
-                    else:
-                        self.createDNSRecord(
-                            type="A",
-                            name=record.name,
-                            value=currentIPv4,
-                            zoneName=zone.name,
-                        )
+                    self._updateSingleDNSRecordLocally(
+                        zoneName=zone.name,
+                        recordName=record.name,
+                        type="A",
+                        value=currentIPv4,
+                    )
             if not self.globalConfig.disable_v6 and currentIPv6Prefix:
                 for record in zone.ipv6_records:
-                    if (
-                        f"AAAA-{record.name}"
-                        in self.zone_records[self.zone_ids[zone.name]]
-                    ):
-                        temp_record = self.zone_records[self.zone_ids[zone.name]][
-                            f"AAAA-{record.name}"
-                        ]
-
-                        ipv6_value = calculateIPv6Address(
+                    self._updateSingleDNSRecordLocally(
+                        zoneName=zone.name,
+                        recordName=record.name,
+                        type="AAAA",
+                        value=calculateIPv6Address(
                             prefix=currentIPv6Prefix,
                             prefixOffset=record.prefixOffset,
-                            currentAddressOrFixedSuffix=(
-                                record.suffix
-                                or self.zone_records[self.zone_ids[zone.name]][
-                                    "AAAA-" + record.name
-                                ].value
-                            ),
-                        )
-
-                        if temp_record.value == ipv6_value:
-                            continue
-
-                        temp_record.value = ipv6_value
-                        if not self.zone_ids[zone.name] in self.updated_zone_records:
-                            self.updated_zone_records[self.zone_ids[zone.name]] = {}
-                        self.updated_zone_records[self.zone_ids[zone.name]][
-                            f"AAAA-{record.name}"
-                        ] = temp_record
-                    else:
-                        self.createDNSRecord(
-                            type="AAAA",
-                            name=record.name,
-                            value=calculateIPv6Address(
-                                prefix=currentIPv6Prefix,
-                                prefixOffset=record.prefixOffset,
-                                currentAddressOrFixedSuffix=record.suffix,
-                            ),
-                            zoneName=zone.name,
-                        )
+                            currentAddressOrFixedSuffix=record.suffix,
+                        ),
+                    )
 
     @abstractmethod
     def updateDNSConfig(self):
         pass
 
+
 class AsyncProvider(Provider):
     aioSession: aiohttp.ClientSession
 
-    def __init__(self, providerConfig: ProviderConfig[Any], globalConfig: GlobalConfig, logger: logging.Logger, aioSession: aiohttp.ClientSession):
+    def __init__(
+        self,
+        providerConfig: ProviderConfig[Any],
+        globalConfig: GlobalConfig,
+        logger: logging.Logger,
+        aioSession: aiohttp.ClientSession,
+    ):
         self.aioSession = aioSession
         super().__init__(providerConfig, globalConfig, logger)
 
